16f2b015bc7e12b134c8bf1adba260cc61dc01bc
thuytien-binhthuan <dtthuytien999@gmail.com>
2025-05-19 23:19:58 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -116,7 +116,7 @@ class Scratch3YourExtension {
                     arguments: {
                         SECONDS: {
                             type: ArgumentType.NUMBER,
-                            defaultValue: 6
+                            defaultValue: 7
                         },
                         LANGUAGE: {
                         type: ArgumentType.STRING,
